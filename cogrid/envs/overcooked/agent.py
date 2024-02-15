e9e1d2fe9bc676e6fc09894f0d0ad22b60487fe7
chasemcd <chasecmcdonald@gmail.com>
2024-02-15 11:18:34 -0500

    remove roles from overcooked agent

--- cogrid/envs/overcooked/agent.py
+++ cogrid/envs/overcooked/agent.py
@@ -2,11 +2,6 @@ from cogrid.core.agent import Agent
 from cogrid.core.grid_object import GridObj
 
 
-class SRRoles:
-    Medic = "medic"
-    Engineer = "engineer"
-
-
 class CookingAgent(Agent):
     def __init__(self, agent_id, start_position, start_direction, **kwargs):
         super().__init__(agent_id, start_position, start_direction, **kwargs)
