a2994e220ec19f73446150f1a1228011e6eb0097
Chase M. <chasecmcdonald@gmail.com>
2025-02-28 04:23:45 +0000

    update to 0.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cogrid",
-    version="0.0.13",
+    version="0.0.14",
     description="Multi-agent extension and expansion of MiniGrid.",
     author="Chase McDonald",
     author_email="chasemcd@cmu.edu",
